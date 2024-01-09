c1c1eeae5a4281989efd02259f1ef1180f1be73d
Dayane <dayanetm@outlook.com>
2024-01-08 22:23:03 -0300

    typescript

---
+++ 03-enums.tsx
@@ -0,0 +1,15 @@
+// Enum
+// Palavras-chaves para auto-complete
+
+// Criando uma lista
+enum linguagens {
+    'Java',
+    'C#',
+    'Python',
+    'Javascript',
+    'Typescript'
+}
+
+// Testar enum
+// Depois do ponto puxa somente os itens da lista
+console.log(linguagens.Typescript);
